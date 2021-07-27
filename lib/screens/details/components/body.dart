54b861a02a337597aa3d23ad1506cc7969830bf8
septiandwianugrah <septiandwi.anugrah@mitrais.com>
2021-07-27 19:41:40 +0700

    Initial commit

---
+++ lib/screens/details/components/body.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_shop/constant.dart';
+import 'package:flutter_shop/models/Product.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'add_to_cart.dart';
+import 'cart_counter.dart';
+import 'color_and_size.dart';
+import 'counter_with_fav_btn.dart';
+import 'description.dart';
+import 'product_title_with_image.dart';
+
+class Body extends StatelessWidget {
+  final Product? product;
+  const Body({Key? key, this.product}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Get total height and width
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          SizedBox(
+            height: size.height,
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(top: size.height * 0.3),
+                  padding: EdgeInsets.only(
+                    top: size.height * 0.12,
+                    left: kDefaultPadding,
+                    right: kDefaultPadding,
+                  ),
+                  // height: 500,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(24),
+                      topRight: Radius.circular(24),
+                    ),
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      ColorAndSize(product: product),
+                      SizedBox(height: kDefaultPadding / 2),
+                      Description(product: product),
+                      SizedBox(height: kDefaultPadding / 2),
+                      CounterWithFavBtn(),
+                      SizedBox(height: kDefaultPadding / 2),
+                      AddToCart(product: product),
+                    ],
+                  ),
+                ),
+                ProductTitleWithImage(product: product),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/home/components/body.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_shop/constant.dart';
+import 'package:flutter_shop/models/Product.dart';
+import 'package:flutter_shop/screens/details/details_screen.dart';
+
+import 'categories.dart';
+import 'item_card.dart';
+
+class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+          child: Text(
+            "Women",
+            style: Theme.of(context)
+                .textTheme
+                .headline5!
+                .copyWith(fontWeight: FontWeight.bold),
+          ),
+        ),
+        Categories(),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 0.75,
+                  mainAxisSpacing: kDefaultPadding,
+                  crossAxisSpacing: kDefaultPadding),
+              itemCount: products.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ItemCard(
+                  product: products[index],
+                  press: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => DetailsScreen(
+                        product: products[index],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
